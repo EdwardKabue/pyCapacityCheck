905f7f3b7f5877c41e1cb6af01a370b9e3a33614
EdwardKabue <kabueedward@yahoo.com>
2023-01-17 22:32:24 +0300

    run du command using subprocess module

--- space_check.py
+++ space_check.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 cmd_arg = sys.argv #list that stores command line arguments
 path = "." #make the current directory the default path
@@ -10,8 +11,13 @@ if(len(cmd_arg) == 2):
 
 file_dir_list = os.listdir(path) #get the list of files and directories
 
-for item in file_dir_list:
-    #print(os.path.abspath(item))
-    if(os.path.isdir(os.path.abspath(item))):
-        print(item)
-    
+du_process = subprocess.run(["du", "-h", "--max-depth=1", path], capture_output=True, text=True)
+
+print(type(du_process.stdout))
+
+#os.chdir(path) #move to the entered directory.
+
+# for item in file_dir_list:
+#     #print(os.path.abspath(item))
+#     if(os.path.isdir(item)):
+#         print(item)
